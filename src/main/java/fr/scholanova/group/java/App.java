26c42e1040c14f1132887234d353d570c808afa4
Dixi KIYINDOU <dixilindez@gmail.com>
2018-01-30 20:48:58 +0100

    Initial commit

---
+++ src/main/java/fr/scholanova/group/java/App.java
@@ -0,0 +1,13 @@
+package fr.scholanova.group.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
