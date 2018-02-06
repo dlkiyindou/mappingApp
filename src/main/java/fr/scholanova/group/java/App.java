431b263f1aa21a442a642dc80118b481ee633539
Dixi KIYINDOU <dixilindez@gmail.com>
2018-02-06 20:18:27 +0100

    All files

--- src/main/java/fr/scholanova/group/java/App.java
+++ src/main/java/fr/scholanova/group/java/App.java
@@ -4,32 +4,54 @@ import javax.sql.DataSource;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.convert.ConversionException;
+import org.springframework.core.convert.support.DefaultConversionService;
+import org.springframework.core.convert.support.GenericConversionService;
 
 import fr.scholanova.group.java.entities.User;
+import fr.scholanova.group.java.tools.convertor.StringToUserConverter;
 
 public class App 
 {
-    public static void main( String[] args )
-    {
-        User user = new User();
-        user.setFirstName("Javier");
-        user.setLastName("Pastore");
-        
-        System.out.println(user);
-        
-        System.out.println(System.getProperty("java.class.path"));
-        
-        ApplicationContext ctx = new ClassPathXmlApplicationContext("Spring/applicationContext.xml");
-        
-        System.out.println(ctx.getApplicationName());
-        User user3 = (User)ctx.getBean(User.class);
-        user3.setFirstName("Diego");
-        user3.setLastName("Simeone");
-        System.out.println(user3);
-        
-        
-        DataSource datasource = (DataSource)ctx.getBean("dataSource");
-        System.out.println(datasource);
-        
-    }
+	public static void main( String[] args )
+	{
+		User user = new User();
+		user.setFirstName("Javier");
+		user.setLastName("Pastore");
+
+		System.out.println(user);
+
+		System.out.println(System.getProperty("java.class.path"));
+
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("Spring/applicationContext.xml");
+
+		System.out.println(ctx.getApplicationName());
+		User user3 = (User)ctx.getBean(User.class);
+		user3.setFirstName("Diego");
+		user3.setLastName("Simeone");
+		System.out.println(user3);
+
+
+		DataSource datasource = (DataSource)ctx.getBean("dataSource");
+		System.out.println(datasource);
+
+
+		GenericConversionService cs = new DefaultConversionService();
+
+		try {
+			Boolean b = cs.convert("yes", Boolean.class);
+			System.out.println(b);
+			
+			Character c = cs.convert(12, Character.class);
+			
+			
+			cs.addConverter(new StringToUserConverter());
+			
+			User user4 = cs.convert(user3.toString(), User.class);
+			
+			System.out.println(user4);
+		} catch (ConversionException e) {
+			System.out.println(e);
+		}
+	}
 }
