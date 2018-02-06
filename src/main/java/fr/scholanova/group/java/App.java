198cb5bc6bb364fc6753c76f4230ef5614726d58
Dixi KIYINDOU <dixilindez@gmail.com>
2018-02-06 01:33:42 +0100

    Configuration context + beans

--- src/main/java/fr/scholanova/group/java/App.java
+++ src/main/java/fr/scholanova/group/java/App.java
@@ -1,13 +1,29 @@
 package fr.scholanova.group.java;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import fr.scholanova.group.java.entities.User;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        User user = new User();
+        user.setFirstName("Javier");
+        user.setLastName("Pastore");
+        
+        System.out.println(user);
+        
+        System.out.println(System.getProperty("java.class.path"));
+        
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("Spring/applicationContext.xml");
+        
+        System.out.println(ctx.getApplicationName());
+        User user3 = (User)ctx.getBean(User.class);
+        user3.setFirstName("Diego");
+        user3.setLastName("Simeone");
+        
+        System.out.println(user3);
     }
 }
